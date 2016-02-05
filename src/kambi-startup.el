2f453d7dce8c445709fc3930b411a62dd84557f9
Michalis Kamburelis <michalis.kambi@gmail.com>
2016-02-05 17:51:16 +0100

    S-f9 to clean here, S-F10 to clean cge.

--- src/kambi-startup.el
+++ src/kambi-startup.el
@@ -774,8 +774,8 @@ parses local variables written in buffer."
 
 (global-set-key (kbd "<f9>") 'kam-compile-immediate)
 (global-set-key (kbd "<C-f9>") 'compile)
-(global-set-key (kbd "<S-f9>") 'kam-clean-cge)
-(global-set-key (kbd "<S-f10>") 'kam-clean-here)
+(global-set-key (kbd "<S-f9>") 'kam-clean-here)
+(global-set-key (kbd "<S-f10>") 'kam-clean-cge)
 
 (global-set-key (kbd "<f4>") 'next-error)
 
