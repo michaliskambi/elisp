753c5de4f08a5e3de5c99207f571cfe1ded848f2
Michalis Kamburelis <michalis.kambi@gmail.com>
2016-02-16 22:55:48 +0100

    Upgrade psvn.el.

--- contrib/psvn/update.sh
+++ contrib/psvn/update.sh
@@ -1,5 +1,6 @@
 wget http://www.xsteve.at/prg/emacs/psvn.el --output-document=psvn.el
 
 # patch from http://www.eaflux.com/
-wget http://www.eaflux.com/psvn/psvn.el.diff --output-document=psvn.el.diff
-patch < psvn.el.diff
+# No longer needed, since psvn.el version "2015-07-20, 21:42:00"
+# wget http://www.eaflux.com/psvn/psvn.el.diff --output-document=psvn.el.diff
+# patch < psvn.el.diff
