faf25c4c167d8275fcf66d0f0b568700bfedd3c0
Michalis Kamburelis <michalis.kambi@gmail.com>
2016-09-25 17:31:15 +0200

    psvn also on github

--- contrib/psvn/update.sh
+++ contrib/psvn/update.sh
@@ -4,3 +4,6 @@ wget http://www.xsteve.at/prg/emacs/psvn.el --output-document=psvn.el
 # No longer needed, since psvn.el version "2015-07-20, 21:42:00"
 # wget http://www.eaflux.com/psvn/psvn.el.diff --output-document=psvn.el.diff
 # patch < psvn.el.diff
+
+# Also on https://github.com/emacsmirror/psvn
+# wget https://raw.githubusercontent.com/emacsmirror/psvn/master/psvn.el --output-document=psvn.el
