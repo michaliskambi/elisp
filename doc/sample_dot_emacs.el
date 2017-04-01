7e8b8bd41ef29524b1d138a8f2f3f7f9cbbcaedd
Michalis Kamburelis <michalis.kambi@gmail.com>
2017-04-01 16:36:52 +0200

    Useful Mac OS X stuff for .emacs

--- doc/sample_dot_emacs.el
+++ doc/sample_dot_emacs.el
@@ -21,10 +21,26 @@
 ;; Initialize Kambi stuff.
 (require 'kambi-startup)
 
-;; set default font
+;; Set default font.
 ;; Following https://www.emacswiki.org/emacs/SetFonts
 ;; and https://www.gnu.org/software/emacs/manual/html_node/emacs/Fonts.html
 ;; (useful on Windows)
 ;;(set-frame-font "Courier New-16")
 ;; (useful on Linux)
 ;;(set-default-font "DejaVu Sans Mono-12")
+;; (useful on MacOSX)
+;;(set-default-font "Menio-24")
+
+;; Use tools from MacPorts, may be useful on Mac OS X if using MacPorts
+;; (setq magit-git-executable "/opt/local/bin/git")
+;; (setq projectile-generic-command "/opt/local/bin/find . -type f -print0")
+;; (setq projectile-git-command "/opt/local/bin/git ls-files -zco --exclude-standard")
+;; (setq projectile-git-ignored-command "/opt/local/bin/git ls-files -zcoi --exclude-standard")
+;; (setq projectile-git-submodule-command  "/opt/local/bin/git submodule --quiet foreach 'echo $path' | tr '\\n' '\\0'")
+;; (setq svn-status-svn-executable "/opt/local/bin/svn")
+
+;; Better dired on Mac OS X.
+;; http://hocuspokus.net/2008/01/a-better-ls-for-mac-os-x/
+;; https://www.gnu.org/software/emacs/manual/html_node/efaq-w32/Dired-ls.html
+;; (setq ls-lisp-use-insert-directory-program t)      ;; use external ls
+;; (setq insert-directory-program "/opt/local/bin/gls") ;; ls program name
