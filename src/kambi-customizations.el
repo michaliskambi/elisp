b91c140450405d439bce0b6a9a5f8c4b9183a1bc
Michalis Kamburelis <michalis.kambi@gmail.com>
2017-05-11 01:38:56 +0100

    packages customize

--- src/kambi-customizations.el
+++ src/kambi-customizations.el
@@ -73,7 +73,9 @@
  '(mmm-submode-decoration-level 1)
  '(org-replace-disputed-keys t)
  '(org-support-shift-select (quote always))
- '(package-selected-packages (quote (auto-complete ag magit helm-projectile)))
+ '(package-selected-packages
+   (quote
+    (unbound undohist counsel-projectile counsel ivy-hydra auto-complete ag magit)))
  '(projectile-globally-ignored-directories
    (quote
     (".idea" ".eunit" ".git" ".hg" ".fslckout" ".bzr" "_darcs" ".tox" ".svn" ".stack-work")))
