1473f5c593cedaa6b85d6af6e47723f2eeb75721
Michalis Kamburelis <michalis.kambi@gmail.com>
2019-06-04 23:25:25 +0200

    my-count-words-region snippet

--- src/tmp.el
+++ src/tmp.el
@@ -965,3 +965,21 @@ CLICK-EVENT should be a mouse-click event."
   (global-set-key (kbd "C-c d <down-mouse-1>")
                   #'brds/describe-char-at-mouse-click)
 ------------------------------------------------------------------------------
+
+;; by Xah Lee
+;; on http://ergoemacs.org/emacs/elisp_count-region.html
+(defun my-count-words-region (posBegin posEnd)
+  "Print number of words and chars in region."
+  (interactive "r")
+  (message "Counting …")
+  (save-excursion
+    (let (wordCount charCount)
+      (setq wordCount 0)
+      (setq charCount (- posEnd posBegin))
+      (goto-char posBegin)
+      (while (and (< (point) posEnd)
+                  (re-search-forward "\\w+\\W*" posEnd t))
+        (setq wordCount (1+ wordCount)))
+
+      (message "Words: %d. Chars: %d." wordCount charCount)
+      )))
