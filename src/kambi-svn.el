c29f837474d93d0a18ac47d492cd9756ed741a70
Michalis Kamburelis <michalis.kambi@gmail.com>
2020-12-31 02:50:36 +0100

    dsvn optional

--- src/kambi-svn.el
+++ src/kambi-svn.el
@@ -3,22 +3,23 @@
 ;; and it doesn't include 'cl, so it may be more maintained.
 ;; See also https://github.com/oliyh/masvn
 
-(require 'dsvn)
+(when (require 'dsvn nil 'noerror)
 
-(defun kam-svn-status-force-kill ()
-  "Run `svn rm --force' on all selected files.
-See `svn-status-marked-files' for what counts as selected."
-  (interactive)
-  (svn-status-rm t))
+  (defun kam-svn-status-force-kill ()
+    "Run `svn rm --force' on all selected files.
+  See `svn-status-marked-files' for what counts as selected."
+    (interactive)
+    (svn-status-rm t))
 
-  ;; (interactive "P")
-  ;; (message "Forcefully removing: %S" (svn-status-get-file-list-names t))
-  ;; (svn-status-create-arg-file (svn-status-get-file-list t))
-  ;; (svn-run t t 'rm "rm" "--force" "--targets" svn-status-temp-arg-file))
+    ;; (interactive "P")
+    ;; (message "Forcefully removing: %S" (svn-status-get-file-list-names t))
+    ;; (svn-status-create-arg-file (svn-status-get-file-list t))
+    ;; (svn-run t t 'rm "rm" "--force" "--targets" svn-status-temp-arg-file))
 
-;; "k" removes file under point, including non-versioned files.
-;; Consistent wit magit.
-(define-key svn-status-mode-map (kbd "k") 'kam-svn-status-force-kill)
-(define-key svn-status-mode-map (kbd "d") 'svn-status-rm)
+  ;; "k" removes file under point, including non-versioned files.
+  ;; Consistent wit magit.
+  (define-key svn-status-mode-map (kbd "k") 'kam-svn-status-force-kill)
+  (define-key svn-status-mode-map (kbd "d") 'svn-status-rm)
+)
 
 (provide 'kambi-svn)
