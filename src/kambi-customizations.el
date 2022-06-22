e363b3087b757c36ae6d06902240ef4e82dcae69
Michalis Kamburelis <michalis.kambi@gmail.com>
2022-06-22 12:28:57 -0600

    Remove comint-password-prompt-regexp customization, try with defaults again

--- src/kambi-customizations.el
+++ src/kambi-customizations.el
@@ -22,8 +22,6 @@
  '(bibtex-comment-start "%%")
  '(blink-matching-paren-distance nil)
  '(c-tab-always-indent nil)
- '(comint-password-prompt-regexp
-   "Kambi put here regexp impossible to match to disable comint-watch-for-password-prompt")
  '(completion-ignored-extensions
    '(".lps" ".compiled" ".bak" ".blend1" ".cmx" ".cmi" ".cmo" ".dll" ".exe" ".~pas" ".ppw" ".ow" ".ppu" ".tpu" ".dcu" ".o" "~" ".lbin" ".so" ".a" ".ln" ".blg" ".bbl" ".elc" ".lof" ".glo" ".idx" ".lot" ".svn/" ".hg/" ".git/" ".bzr/" "CVS/" "_darcs/" "_MTN/" ".fmt" ".tfm" ".class" ".fas" ".lib" ".mem" ".x86f" ".sparcf" ".dfsl" ".pfsl" ".d64fsl" ".p64fsl" ".lx64fsl" ".lx32fsl" ".dx64fsl" ".dx32fsl" ".fx64fsl" ".fx32fsl" ".sx64fsl" ".sx32fsl" ".wx64fsl" ".wx32fsl" ".fasl" ".ufsl" ".fsl" ".dxl" ".lo" ".la" ".gmo" ".mo" ".toc" ".aux" ".cp" ".fn" ".ky" ".pg" ".tp" ".vr" ".cps" ".fns" ".kys" ".pgs" ".tps" ".vrs" ".pyc" ".pyo"))
  '(copyright-names-regexp "Michalis Kamburelis")
