6cd27a981ba61847e3d9af9b81b58d2a6ecc2b4d
Michalis Kamburelis <michalis.kambi@gmail.com>
2023-02-28 05:49:43 +0100

    Beter pasls comments and defaults

--- lsp/kambi-pascal-lsp.el
+++ lsp/kambi-pascal-lsp.el
@@ -22,46 +22,54 @@
 (require 'lsp-pascal)
 
 ;; configure lsp-pascal variables
-;;
-;; Note: CGE pasls will autodetect OS/CPU, if they are not specified explicitly, based on current platform.
-;; Note: Philip Zander LSP will try to autodetect it looking at Lazarus config too.
-;; So you don't strictly need to specify them.
 (if kam-is-windows
     (progn
       (setq lsp-pascal-command "D:/cygwin64/home/michalis/sources/castle-engine/castle-engine/bin/pasls.exe")
+      ;; some alternatives:
       ;;(setq lsp-pascal-command "c:/Program Files (x86)/Embarcadero/Studio/22.0/bin/DelphiLSP.exe")
 
       (setq lsp-pascal-fpcdir "D:/cygwin64/home/michalis/installed/fpclazarus/3.2.2-lazarus2.2/fpcsrc/")
       (setq lsp-pascal-lazarusdir "D:/cygwin64/home/michalis/installed/fpclazarus/3.2.2-lazarus2.2/lazarus")
       (setq lsp-pascal-pp "D:/cygwin64/home/michalis/installed/fpclazarus/3.2.2-lazarus2.2/fpc/bin/x86_64-win64/fpc.exe")
-      (setq lsp-pascal-fpctarget "win64")
-      (setq lsp-pascal-fpctargetcpu "x86_64")
+
+      ;; You probably don't need to specify lsp-pascal-fpctarget / lsp-pascal-fpctargetcpu explicitly.
+      ;; - CGE pasls will autodetect OS/CPU, if they are not specified explicitly, based on current platform.
+      ;; - Philip Zander LSP will try to autodetect it looking at Lazarus config too.
+      ;(setq lsp-pascal-fpctarget "win64")
+      ;(setq lsp-pascal-fpctargetcpu "x86_64")
     )
 
   (progn
-    ;;(setq lsp-pascal-command "/home/michalis/sources/lsp/pascal-language-server/server/lib/x86_64-linux/pasls")
 
-    ;; mkdir -p ~/sources/lsp
-    ;; cd ~/sources/lsp
-    ;; git clone https://github.com/castle-engine/pascal-language-server castle-isopod-pascal-language-server
-    ;; cd castle-isopod-pascal-language-server
+    ;; quick installation instructions:
+    ;;
+    ;; mkdir -p ~/sources/castle-engine
+    ;; git clone https://github.com/castle-engine/pascal-language-server
+    ;; cd pascal-language-server
     ;; git submodule update --init --recursive
+    ;; mr register # if using myrepos
     ;; lazbuild server/pasls.lpi
     ;;
     ;; mkdir -p ~/.config/pasls/
     ;; emacs ~/.config/pasls/castle-pasls.ini # use sample from https://github.com/castle-engine/pascal-language-server, define CGE path
-    (setq lsp-pascal-command "/home/michalis/sources/lsp/castle-isopod-pascal-language-server/server/pasls")
 
+    (setq lsp-pascal-command "/home/michalis/sources/castle-engine/pascal-language-server/server/pasls")
+
+    ;; some alternatives:
     ;;(setq lsp-pascal-command "/home/michalis/sources/lsp/castle-genericptr-pascal-language-server/lib/x86_64-linux/pasls")
+    ;;(setq lsp-pascal-command "/home/michalis/sources/lsp/pascal-language-server/server/lib/x86_64-linux/pasls")
 
-    ;; Should this lead to /home/michalis/installed/fpclazarus/current/fpc or fpcsrc?
-    ;; Description suggests it's for source, name suggests it's passed to FPC so it should contain just compiled units.
-    ;; Later: Message on FPC mailing lists confirms it's for source.
+    ;; lsp-pascal-fpcdir should lead to sources (../fpcsrc in case of installation done by fpcupdeluxe), not ../fpc .
+    ;; Tests and message on FPC mailing lists confirms it's for source.
     (setq lsp-pascal-fpcdir "/home/michalis/installed/fpclazarus/current/fpcsrc/")
     (setq lsp-pascal-lazarusdir "/home/michalis/installed/fpclazarus/current/lazarus")
     (setq lsp-pascal-pp "/home/michalis/installed/fpclazarus/current/fpc/bin/x86_64-linux/fpc.sh")
-    (setq lsp-pascal-fpctarget "linux")
-    (setq lsp-pascal-fpctargetcpu "x86_64")
+
+    ;; You probably don't need to specify lsp-pascal-fpctarget / lsp-pascal-fpctargetcpu explicitly.
+    ;; - CGE pasls will autodetect OS/CPU, if they are not specified explicitly, based on current platform.
+    ;; - Philip Zander LSP will try to autodetect it looking at Lazarus config too.
+    ;(setq lsp-pascal-fpctarget "linux")
+    ;(setq lsp-pascal-fpctargetcpu "x86_64")
   )
 )
 
